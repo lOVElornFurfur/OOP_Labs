de0d99c61aa6fe25197fd621b0d5d19b1b63f76d
Dmitry <31744456+lOVElornFurfur@users.noreply.github.com>
2017-11-24 01:40:53 +0300

    Add files via upload

---
+++ Lab9/Lab9/Boss.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Boss
+    {
+        public delegate void MachineStateHandler(int voltage);
+        public delegate void UpgradeStateHandler(int upgradeToINT);
+
+        public event MachineStateHandler OnTurnOn;
+        public event UpgradeStateHandler OnUpgrade;
+
+        public void Upgrade(int upgradeToINT)
+        {
+            OnUpgrade(upgradeToINT);
+        }
+        public void TurnON(int voltage)
+        {
+            OnTurnOn(voltage);
+        }
+
+    }
+}
